3faa2b69a1c599e8029d5572dd25d52051692cf0
elijahcdavid <elijah.david@outlook.com>
2025-02-20 16:26:09 -0500

    added age to the patients table

--- sql/diabetes_dashboard.sql
+++ sql/diabetes_dashboard.sql
@@ -11,20 +11,23 @@ diabetes_diagnosis as
 	where code = '44054006'
 	group by condition.patient
 )
-
 select patient.id,
 	patient.birth_date,
 	patient.deceased_date,
+	DATE_PART('year', AGE(patient.birth_date::DATE)) as age,
 	patient.first_name,
 	patient.last_name,
 	patient.gender,
 	patient.race,
 	patient.zip,
 	diabetes.earliest_diabetes_diagnosis,
-	1 as diabetes_diagnosis
+	case 
+		when diabetes.earliest_diabetes_diagnosis is null then 0
+		else 1
+	end as diabetes_diagnosis
 from public.patient as patient
 
 join active_patients as active 
 	on patient.id = active.id
-inner join diabetes_diagnosis as diabetes
+left join diabetes_diagnosis as diabetes
 	on patient.id = diabetes.patient
